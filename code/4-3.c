1ab1e67d2dc206bdcd835c0d06a8cfc04483124a
sodk6609 <sodk6609@gmail.com>
2026-03-12 12:10:18 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-
-int main()
-{
-
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    int is_prime = 1;
+    if (n < 2) {
+        is_prime = 0;
+    } else {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                is_prime = 0;
+                break;
+            }
+        }
+    }
+    if (is_prime) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
     return 0;
 }
-
